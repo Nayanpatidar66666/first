5ef98b2f164c56e4463554be2a3bbc2ac4ba8054
Nayan Patidar <patidarnayan7074@gmail.com>
2024-11-27 08:52:37 +0530

    Your hearte

---
+++ oop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ class Hello{
+    Hello(){
+System.out.println("not parmeters ");
+    }
+    Hello(int a){
+        System.out.println("hello"+a);
+    }
+void hell(int a){
+    System.out.println("nayan"+a);
+}
+
+}
+public class oop{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        Hello h=new Hello();
+        new Hello(x);
+h.hell(x);
+    }
+}
